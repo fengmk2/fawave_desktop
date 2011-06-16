6b760165151a81c5491c7a6fb85891c4c36851cb
fengmk2 <fengmk2@gmail.com>
2011-06-17 02:11:39 +0800

    增加加密和解密函数

--- Resources/fawave/js/utils/fawave.util.js
+++ Resources/fawave/js/utils/fawave.util.js
@@ -1,3 +1,19 @@
+/**
+ * utils function use in fawave
+ */
+
+(function(){
+
+var Base64 = weibo.base64.Base64;
 window.FaWave = window.FaWave || {};
 FaWave.Util = {
-};
+	secret_key: '523f2d0d134bfd5aa138f9e5af828bf9',
+	encrypt: function(s) {
+		return Base64.strcode(s, this.secret_key);
+	},
+	decrypt: function(s) {
+		return Base64.strcode(s, this.secret_key, true);
+	}
+};	
+
+})();
--- doc/dev_envirment.md
+++ doc/dev_envirment.md
@@ -9,6 +9,6 @@ TODO...
 依赖的各大微博访问库
 
     $ cd $PROJECT_DIR/Resources/lib
-    # 如果发现 $PROJECT_DIR/Resources/lib/weibo
+    # 如果发现 $PROJECT_DIR/Resources/lib/weibo 不存在，则
     $ git clone https://github.com/fengmk2/node-weibo.git weibo
 
